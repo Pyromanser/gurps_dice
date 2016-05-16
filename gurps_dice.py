296837ace7b6890750e8795716cdf34af912be29
Yaroslav <yaroslav@code-on.be>
2016-05-16 18:05:14 +0300

    Ref code

--- gurps_dice.py
+++ gurps_dice.py
@@ -31,12 +31,19 @@ class Dice(object):
     Dice("3d6") + Dice("1d6") -> Dice("2d6")
     """
 
-    def __init__(self, dice_str=None):
-        if dice_str:
-            founded_dices = self.search_dice_in_str(dice_str)
+    def __init__(self, count=0, base=0):
+        if not isinstance(count, int):
+            dice_dict = self.search_dice_in_str(count)
         else:
-            founded_dices = {'count': 0, 'base': 0}
-        self.count, self.base = founded_dices['count'], founded_dices['base']
+            dice_dict = {'count': count, 'base': base}
+        if self._is_count_valid(count=dice_dict['count']):
+            self.count = dice_dict['count']
+        if self._is_base_valid(base=dice_dict['base']):
+            self.base = dice_dict['base']
+
+    def __call__(self, count=0, base=0):
+        self.__init__(count=count, base=base)
+        return self
 
     def __str__(self):
         dice_str = "{:d}d{:d}".format(self.count, self.base)
@@ -49,9 +56,7 @@ class Dice(object):
         if isinstance(other, self.__class__):
             if self.base != other.base:
                 raise BaseDiceError("there is different dice base")
-            dice = Dice()
-            dice.set_base(self.base)
-            dice.set_count(self.count + other.count)
+            dice = Dice(count=self.count + other.count, base=self.base)
             return dice
         else:
             raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(self.__class__, type(other)))
@@ -60,9 +65,7 @@ class Dice(object):
         if isinstance(other, self.__class__):
             if self.base != other.base:
                 raise BaseDiceError("there is different dice base")
-            dice = Dice()
-            dice.set_base(self.base)
-            dice.set_count(self.count - other.count)
+            dice = Dice(count=self.count - other.count, base=self.base)
             return dice
         else:
             raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(self.__class__, type(other)))
@@ -71,66 +74,68 @@ class Dice(object):
     @staticmethod
     def search_dice_in_str(dice_str):
         if not isinstance(dice_str, str):
-            raise TypeError("unsupported operand type for search_dice_in_str: '{}'".format(type(dice_str)))
-        dice_search = re.match(
-            r"^(?P<count>\d+)d(?P<base>\d+)$",
-            dice_str
-        )
+            try:
+                dice_str = str(dice_str)
+            except TypeError:
+                raise TypeError("unsupported operand type for search_dice_in_str: '{}'".format(type(dice_str)))
+        dice_search = re.match(r"^(?P<count>\d+)d(?P<base>\d+)$", dice_str)
         if dice_search:
             return {k: int(v) for k, v in dice_search.groupdict().items()}
         else:
             raise EmptyDiceError("there is no correct dice param in str")
 
-    def set_dice(self, dice_str):
+    def set_dice(self, count, base=0):
         """
         Run init for set Dice
         """
-        self.__init__(dice_str)
+        self.__init__(count=count, base=base)
 
-    def set_base(self, base):
+    def set_base(self, base=0):
         """
         Set dice base for Dice
         """
-        if not isinstance(base, int):
-            raise TypeError("unsupported operand type for set_base: '{}'".format(type(base)))
-        elif base < 0:
-            raise BaseDiceError("dice base can not be less than zero")
-        self.base = base
+        if self._is_base_valid(base=base):
+            self.base = base
 
-    def set_count(self, count):
+    def set_count(self, count=0):
         """
         Set count of dice for Dice
         """
-        if not isinstance(count, int):
-            raise TypeError("unsupported operand type for set_base: '{}'".format(type(count)))
-        elif count < 0:
-            raise CountDiceError("dice count can not be less than zero")
-        self.count = count
+        if self._is_count_valid(count=count):
+            self.count = count
 
     # Validating
-    def _is_count_valid(self):
+    @staticmethod
+    def _is_count_valid(count):
         """
         Check Dice for validity by dice count.
         Return True if it's Ok
         Rise error's if something not Ok
         """
-        if not isinstance(self.count, int):
-            raise CountDiceError("unsupported type for dice count: '%s'" % type(self.count))
-        elif self.count < 0:
+        if not isinstance(count, int):
+            raise CountDiceError("unsupported type for dice count: '%s'" % type(count))
+        elif count < 0:
             raise CountDiceError("dice count can not be less than zero")
         return True
 
-    def _is_base_valid(self):
+    def _is_self_count_valid(self):
+        return self._is_count_valid(count=self.count)
+
+    @staticmethod
+    def _is_base_valid(base):
         """
         Check Dice for validity by dice base.
         Return True if it's Ok
         Rise error's if something not Ok
         """
-        if not isinstance(self.base, int):
-            raise BaseDiceError("unsupported type for dice base: '%s'" % type(self.base))
-        elif self.base < 0:
+        if not isinstance(base, int):
+            raise BaseDiceError("unsupported type for dice base: '%s'" % type(base))
+        elif base < 0:
             raise BaseDiceError("dice base can not be less than zero")
         return True
+
+    def _is_self_base_valid(self):
+        return self._is_base_valid(base=self.base)
 
     def _is_dice_valid(self):
         """
@@ -138,7 +143,7 @@ class Dice(object):
         Return True if it's Ok
         Rise error's if something not Ok
         """
-        if self._is_count_valid() and self._is_base_valid():
+        if self._is_self_count_valid() and self._is_self_base_valid():
             return True
 
     # Dice roll functionality
@@ -171,6 +176,8 @@ class GurpsDice(Dice):
     """
     It makes it possible to operate with dices with base equal 6.
     <count>d6±<bonus>
+    or
+    <count>d6
     On the addition of number increases the bonus. Similarly, when subtracting.
     GurpsDice("1d6+1") + 1 -> GurpsDice("1d6+2")
     GurpsDice("1d6+1") - 2 -> GurpsDice("1d6-1")
@@ -179,13 +186,22 @@ class GurpsDice(Dice):
     GurpsDice("3d6+3") - GurpsDice("1d6-1") -> GurpsDice("2d6+4")
     """
 
-    def __init__(self, dice_str=None):
-        super(GurpsDice, self).__init__(dice_str=None)
-        if dice_str:
-            founded_dices = self.search_dice_in_str(dice_str)
+    def __init__(self, count=1, base=6, bonus=0):
+        super(GurpsDice, self).__init__(count=0, base=0)
+        if not isinstance(count, int):
+            dice_dict = self.search_dice_in_str(count)
         else:
-            founded_dices = {'count': 1, 'base': 6, 'bonus': 0}
-        self.count, self.base, self.bonus = founded_dices['count'], founded_dices['base'], founded_dices['bonus']
+            dice_dict = {'count': count, 'base': base, 'bonus': bonus}
+        if self._is_count_valid(count=dice_dict['count']):
+            self.count = dice_dict['count']
+        if self._is_base_valid(base=dice_dict['base']):
+            self.base = dice_dict['base']
+        if self._is_bonus_valid(bonus=dice_dict['bonus']):
+            self.bonus = dice_dict['bonus']
+
+    def __call__(self, count=1, base=6, bonus=0):
+        self.__init__(count=count, base=base, bonus=bonus)
+        return self
 
     def __str__(self):
         dice_str = super(GurpsDice, self).__str__()
@@ -193,39 +209,35 @@ class GurpsDice(Dice):
             dice_str += "{:+d}".format(self.bonus)
         return dice_str
 
+    def __repr__(self):
+        return "GurpsDice({})".format(self.__str__())
+
     def __add__(self, other):
-        dice = GurpsDice()
-        dice.set_base(self.base)
         if isinstance(other, self.__class__):
-            dice.set_count(self.count + other.count)
-            dice.set_bonus(self.bonus + other.bonus)
-            return dice
+            dice = GurpsDice(count=self.count + other.count, bonus=self.bonus + other.bonus)
         elif isinstance(other, int):
-            dice.set_count(self.count)
-            dice.set_bonus(self.bonus + other)
-            return dice
+            dice = GurpsDice(count=self.count, bonus=self.bonus + other)
         else:
             raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(self.__class__, type(other)))
+        return dice
 
     def __sub__(self, other):
-        dice = GurpsDice()
-        dice.set_base(self.base)
         if isinstance(other, self.__class__):
-            dice.set_count(self.count - other.count)
-            dice.set_bonus(self.bonus - other.bonus)
-            return dice
+            dice = GurpsDice(count=self.count - other.count, bonus=self.bonus - other.bonus)
         elif isinstance(other, int):
-            dice.set_count(self.count)
-            dice.set_bonus(self.bonus - other)
-            return dice
+            dice = GurpsDice(count=self.count, bonus=self.bonus - other)
         else:
             raise TypeError("unsupported operand type(s) for +: '{}' and '{}'".format(self.__class__, type(other)))
+        return dice
 
     # Setting
     @staticmethod
     def search_dice_in_str(dice_str):
         if not isinstance(dice_str, str):
-            raise TypeError("unsupported operand type for search_dice_in_str: '{}'".format(type(dice_str)))
+            try:
+                dice_str = str(dice_str)
+            except TypeError:
+                raise TypeError("unsupported operand type for search_dice_in_str: '{}'".format(type(dice_str)))
         dice_search = re.match(
             r"^(?P<count>\d+)d(?P<base>[6])(?P<bonus>[-,+]\d+)?$",
             dice_str
@@ -235,43 +247,60 @@ class GurpsDice(Dice):
         else:
             raise EmptyDiceError("there is no correct dice param in str")
 
-    def set_base(self, base):
+    def set_dice(self, count, base=6, bonus=0):
         """
-        Set dice base for GurpsDice
+        Run init for set Dice
         """
-        if base != 6:
-            raise BaseDiceError("dice base should be equal to 6")
-        self.base = base
+        self.__init__(count=count, base=base, bonus=bonus)
+
+    def set_base(self, base=6):
+        """
+        Set dice base for Dice
+        """
+        if self._is_base_valid(base=base):
+            self.base = base
+
+    def set_count(self, count=1):
+        """
+        Set count of dice for Dice
+        """
+        super(GurpsDice, self).set_count(count=count)
 
     def set_bonus(self, bonus):
         """
         Set bonus for GurpsDice
         """
-        if not isinstance(bonus, int):
-            raise TypeError("unsupported operand type for set_base: '{}'".format(type(bonus)))
-        self.bonus = bonus
+        if self._is_bonus_valid(bonus=bonus):
+            self.bonus = bonus
 
     # Validating
-
-    def _is_base_valid(self):
+    @staticmethod
+    def _is_base_valid(base):
         """
         Check Dice for validity by dice base.
         Return True if it's Ok
         Rise error's if something not Ok
         """
-        if self.base != 6:
+        if base != 6:
             raise BaseDiceError("dice base should be equal to 6")
         return True
 
-    def _is_bonus_valid(self):
+    def _is_self_base_valid(self):
+        return self._is_base_valid(base=self.base)
+
+    @staticmethod
+    def _is_bonus_valid(bonus):
         """
         Check GurpsDice for validity by bonus.
         Return True if it's Ok
         Rise error's if something not Ok
         """
-        if not isinstance(self.bonus, int):
-            raise BonusDiceError("unsupported type for dice base: '%s'" % type(self.bonus))
+        if not isinstance(bonus, int):
+            raise BonusDiceError("unsupported type for dice base: '%s'" % type(bonus))
         return True
+
+    def _is_self_bonus_valid(self):
+        return self._is_bonus_valid(bonus=self.bonus)
 
     def _is_dice_valid(self):
         """
@@ -280,7 +309,7 @@ class GurpsDice(Dice):
         Rise error's if something not Ok
         """
         base_and_count_valid = super(GurpsDice, self)._is_dice_valid()
-        if base_and_count_valid and self._is_bonus_valid():
+        if base_and_count_valid and self._is_self_bonus_valid():
             return True
 
     # GurpsDice roll functionality
